3a867f483c0a9c5cdca5d8b7791bea16ad7e3390
Iana Barkhatova <yanabarhat@gmail.com>
2025-01-10 14:30:43 +0200

    create Todolist component, pass title prop to Todolist; replaced default import for App with named import

---
+++ src/components/Todolist.tsx
@@ -0,0 +1,34 @@
+export const Todolist = (props: Todolist) => {
+
+    const {title} = props
+
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={true}/> <span>JS</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={false}/> <span>React</span>
+                </li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
+
+type Todolist = {
+    title: string
+}
